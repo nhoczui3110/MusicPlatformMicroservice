afbc77317dab5573c7c98925bd1eded46ad6392c
nhoczui3110 <tranvietquang3110@gmail.com>
2025-02-22 21:28:22 +0700

    init project

---
+++ user-library-service/src/test/java/com/MusicPlatForm/user_library_service/UserLibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MusicPlatForm.user_library_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
