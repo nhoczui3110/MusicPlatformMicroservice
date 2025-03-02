7ed88e4c93a99c567d0cd5b6069acbf803745595
Phuong22122003 <hnguyenphuong09@gmail.com>
2025-03-02 15:55:11 +0700

    update: file-service

---
+++ file-service/src/main/java/com/MusicPlatForm/file_service/dto/response/CoverResponse.java
@@ -0,0 +1,17 @@
+package com.MusicPlatForm.file_service.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class CoverResponse {
+    String coverName;    
+}
