138948945f262a30e5a20598c27a2420628c1074
nhoczui3110 <tranvietquang3110@gmail.com>
2025-06-05 21:58:53 +0700

    Push k8s files

--- identity-service/src/main/java/com/example/identity_service/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/example/identity_service/IdentityServiceApplication.java
@@ -1,15 +1,18 @@
 package com.example.identity_service;
 
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableFeignClients
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IdentityServiceApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(IdentityServiceApplication.class, args);
+
 	}
 
 }
